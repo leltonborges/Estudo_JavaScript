cb984489a21245987602ae588c9a97a5897a9034
Lelton Borges <lelton.borges1@gmail.com>
2020-08-07 16:09:22 -0300

    Tipo string

---
+++ Fundamentos/string.js
@@ -0,0 +1,11 @@
+const escola = 'Estudos'
+
+console.log(escola.charAt(4))
+console.log(escola.charAt(7))
+console.log(escola.charCodeAt(2))
+console.log(escola.indexOf('t'))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0,5))
+
+console.log('Escola '.concat(escola).concat('!'))
